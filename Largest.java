536f9fb6c1e62cd852b717df3c80138c9113c8c8
GSNadarajan <nadarajangs31@gmail.com>
2024-01-01 11:00:36 +0530

    feat : Conditional statement

---
+++ Largest.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Largest{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter three integers: ");
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int max = a;
+        if(b > max){
+            max = b;
+        }
+        if(c > max){
+            max = c;
+        }
+        System.out.println(max + " is the largest among three");
+        in.close();
+    }
+}
