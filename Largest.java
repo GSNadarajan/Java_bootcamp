266db77a5185397ebb08273d1ede0e5c649202fc
GSNadarajan <nadarajangs31@gmail.com>
2024-01-01 11:18:05 +0530

    feat : Casecheck program

--- Largest.java
+++ Largest.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Largest{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter three integers: ");
+        System.out.println("Enter three integer s: ");
         int a = in.nextInt();
         int b = in.nextInt();
         int c = in.nextInt();
