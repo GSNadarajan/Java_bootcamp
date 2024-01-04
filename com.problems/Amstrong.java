4591d13cac0e13ac9650e33547bf64cebf256213
GSNadarajan <nadarajangs31@gmail.com>
2024-01-04 22:21:23 +0530

    feat: Amstrong number

---
+++ com.problems/Amstrong.java
@@ -0,0 +1,21 @@
+public class Amstrong {
+    public static void main(String[] args) {
+        int num = 153;
+        int original = num;
+
+        int result = 0;
+
+        while(num > 0){
+            int rem = num % 10;
+            result = result + rem * rem * rem;
+            num = num /10;
+        }
+        if(original == result){
+            System.out.println(result + " is an amstrong number");
+        }
+        else{
+            System.out.println(result + " is not an amstrong number");
+        }
+    }
+    
+}
