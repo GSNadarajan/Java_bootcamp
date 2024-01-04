12a184819b09e1d6e592b3813bc92e52d137ddcf
GSNadarajan <nadarajangs31@gmail.com>
2024-01-04 14:29:08 +0530

    feat : variable length argument

---
+++ com.function/VarArgs.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class VarArgs {
+    // In java Variable length arguments is simply we can pass n input 
+    //of input data based on the datatype... But it should follow the 
+    //order while taking the values from the parameter
+
+    public static void main(String[] args) {
+       varArgs(1,2,3,4);
+        
+    }
+
+    static void varArgs(int a, int ...v){
+        System.out.println("The value of a: " + a);
+        System.out.println(Arrays.toString(v));
+
+    }
+
+}
