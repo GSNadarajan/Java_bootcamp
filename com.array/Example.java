4b3ed64249c91b374d431ebf03798f588aabbcd7
GSNadarajan <nadarajangs31@gmail.com>
2024-01-06 22:45:04 +0530

    feat: array indexing

---
+++ com.array/Example.java
@@ -0,0 +1,25 @@
+public class Example{
+    public static void main(String[] args) {
+        
+        int[] roll_no = { 1,2,3,4}; // Array declaration with object initialization
+        System.out.println(roll_no[2]);
+
+        // int[] age;
+        // age = new int[3];
+        int[] age = new int[4];
+        System.out.println("The value of age is : " + age[3]);  // 0
+
+
+        int[] no = { 1,2,3,4};
+        System.out.println("The value of no is: " + no[2]);  // 3
+
+        String[] name = new String[3];
+        System.out.println("Accessing the empty string array value: " + name[2]);
+
+
+        
+
+
+
+    }
+}
