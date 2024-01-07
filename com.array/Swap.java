b1ab580d4c1e4b1f6c3674d1e5d0e90a330b8ed2
GSNadarajan <nadarajangs31@gmail.com>
2024-01-07 15:48:05 +0530

    feat : ML ArrayList and swap problem

---
+++ com.array/Swap.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class Swap {
+    public static void main(String[] args) {
+        
+        int[] arr = {1,2,3,4,5};
+        System.out.println("Before getting swapped: " + Arrays.toString(arr));
+        int[] result = swap(arr, 0, 1);
+        System.out.println("After getting swapped: "+ Arrays.toString(result));
+        
+    }
+
+    static int[] swap(int[] arr, int index1, int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+        return arr;
+    }
+    
+}
