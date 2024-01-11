1f9236187e165716464bef6b9f7d97d35cd23c2a
GSNadarajan <nadarajangs31@gmail.com>
2024-01-11 14:17:08 +0530

    feat : Three sum problem

---
+++ com.leetcode/ThreeSum.java
@@ -0,0 +1,41 @@
+
+import java.util.*;
+
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        if (nums.length == 0 || nums.length < 3) return new ArrayList<>();
+        Arrays.sort(nums);
+        Set<List<Integer>> result = new HashSet<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return new ArrayList<>(result);
+    }
+
+    public static void main(String[] args) {
+        ThreeSum solution = new ThreeSum();
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> result = solution.threeSum(nums);
+
+        System.out.println("ThreeSum results:");
+        for (List<Integer> triplet : result) {
+            System.out.println(triplet);
+        }
+    }
+}
