0cf6b5f751f6e4bf28f09a9c4d85262a4fbc38fe
GSNadarajan <nadarajangs31@gmail.com>
2024-01-17 21:59:54 +0530

    feat: Ceiling problem

---
+++ com.binarySearch/Ceiling.java
@@ -0,0 +1,18 @@
+public class Ceiling {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+        int target = 4;
+        // TO find the nearest element to the target which is greater than the target
+    }
+
+    // static int ceil(int[] arr, int target){
+    //     int start = 0;
+    //     int end = arr.length - 1;
+    //     int mid = start + end - start/2;
+
+    //     while(start < end){
+
+    //     }
+    // }
+    
+}
