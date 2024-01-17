e256f3c0884fa362a1880a448ee2739c457d7313
GSNadarajan <nadarajangs31@gmail.com>
2024-01-17 22:14:34 +0530

    feat: updated Ceiling code

--- com.binarySearch/Ceiling.java
+++ com.binarySearch/Ceiling.java
@@ -1,34 +1,28 @@
 public class Ceiling {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7};
-        int target = 4;
-        // TO find the nearest element to the target which is greater than the target
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int target = 5;
+        // To find the nearest element to the target which is greater than or equal to the target
         int result = ceil(arr, target);
         System.out.println(result);
     }
 
-    static int ceil(int[] arr, int target){
+    static int ceil(int[] arr, int target) {
         int start = 0;
         int end = arr.length - 1;
-       
 
-        while(start <= end){
-            int mid = start + (end - start) /2;
+        int result = -1; // Initialize result to an invalid value
 
-            if(target < arr[mid]){
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target <= arr[mid]) {
                 result = mid; // Update result and continue search in the left subarray
                 end = mid - 1;
-            }
-            else if(target > arr[mid]){
+            } else {
                 start = mid + 1;
             }
-            else{
-                return mid;
-            }
-
-
         }
-        return start;
+        return result;
     }
-    
 }
