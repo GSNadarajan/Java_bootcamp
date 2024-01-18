9c9aeed002d451a90682fe881acd401912153a81
GSNadarajan <nadarajangs31@gmail.com>
2024-01-18 19:59:10 +0530

    feat: Ceiling reupdated:

---
+++ com.BinaryProblem/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        // When do we apply binary search ?
+        // When an array is sorted then 95% we can apply sorted array
+
+        // Qn: Ceiling of a number
+        //int[] arr = {1,2,3,4,5,6,8}; // This array is in sorted, so we can use binary search
+        // What is ceiling = smallest element in array greater than or = target
+        
+        // ceiling(arr, target= 6) = 6
+        // ceiling(arr, target = 7) = 8
+
+          
+
+
+
+    }
+}
