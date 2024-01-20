1bc38eeaeeed1512d2605d6d66a37f2194675ace
GSNadarajan <nadarajangs31@gmail.com>
2024-01-20 22:01:56 +0530

    feat : Leetcode 34 problem updated

--- com.BinaryProblem/Leetcode_34.java
+++ com.BinaryProblem/Leetcode_34.java
@@ -4,6 +4,8 @@ public class Leetcode_34 {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 4, 5, 6};
         int target = 4;
+
+        
         
         int[] result = searchRange(arr, target);
         
@@ -12,9 +14,14 @@ public class Leetcode_34 {
         }
     }
 
+    
+
     public int[] searchRange(int[] arr, int target){
         int start = 0;
         int end = arr.length - 1;
+        // if no element is found
+
+        int[] ans = {-1, -1};
 
         while(start <= end){
             // find the middle element
@@ -22,8 +29,18 @@ public class Leetcode_34 {
             
             int mid = start + (end - start) /2;
 
-            if(target )
+            if(target < arr[mid]){
+                end = mid - 1;
+            }
+            else if (target < arr[mid]){
+                start = mid + 1;
+            }
+            else{
+                return mid;
+            }
         }
+        return ans;
+        
     }
 
 
