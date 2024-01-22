32638901a98d1db9c312da552c9cfd847ccf40cd
GSNadarajan <nadarajangs31@gmail.com>
2024-01-22 22:06:43 +0530

    feat : Climbing start #LC70

---
+++ com.leetcode/Leetcode_70.java
@@ -0,0 +1,29 @@
+public class Leetcode_70 {
+    // https://leetcode.com/problems/climbing-stairs/description/
+    public static void main(String[] args) {
+        System.out.println(climbStairs(2)); // Output: 2
+        System.out.println(climbStairs(3)); // Output: 3
+        System.out.println(climbStairs(6));
+    }
+
+    public static int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
+   
+}
