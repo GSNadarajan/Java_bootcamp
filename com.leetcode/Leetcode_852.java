0c00229b0eee17e6261d340c34083d8bd3d3e8fb
GSNadarajan <nadarajangs31@gmail.com>
2024-01-24 22:28:30 +0530

    feat : updated Leetcode852.java

---
+++ com.leetcode/Leetcode_852.java
@@ -0,0 +1,25 @@
+public class Leetcode_852 {
+    public static void main(String[] args) {
+        int[] arr = {0,10,5,2};
+        System.out.println(peakIndexInMountainArray(arr));
+    }
+
+    public static int peakIndexInMountainArray(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start < end){
+            int mid = start + (end - start + 1)/ 2;
+
+            if(arr[mid] > arr[mid + 1]){
+                end = mid;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+
+        return start;
+    }
+    
+}
