dc0f7f8b658fa71e57521735c8acc9b24f8a114c
GSNadarajan <nadarajangs31@gmail.com>
2024-01-25 16:29:26 +0530

    feat : practising peak index mountain problem

--- com.leetcode/Leetcode_852.java
+++ com.leetcode/Leetcode_852.java
@@ -1,6 +1,7 @@
 public class Leetcode_852 {
     public static void main(String[] args) {
         int[] arr = {0,10,5,2};
+                 //  0 1 2  3
         System.out.println(peakIndexInMountainArray(arr));
     }
 
@@ -9,16 +10,17 @@ public class Leetcode_852 {
         int end = arr.length - 1;
 
         while(start < end){
-            int mid = start + (end - start + 1)/ 2;
+            int mid = start + (end - start )/ 2;
 
             if(arr[mid] > arr[mid + 1]){
                 end = mid;
+
             }
             else{
                 start = mid + 1;
             }
-        }
 
+        }
         return start;
     }
     
