353174d910983e5e485390bff8ac88dd1a38e5ea
GSNadarajan <nadarajangs31@gmail.com>
2024-01-27 13:16:03 +0530

    feat : updated peak index

--- com.leetcode/Leetcode_162.java
+++ com.leetcode/Leetcode_162.java
@@ -3,6 +3,20 @@ public class Leetcode_162 {
         //https://leetcode.com/problems/find-peak-element/description/
         
         int[] arr = {1, 2, 3, 1 };
+        //First iteration
+        //start = index(0) , end = index(3) (0+3/2 = 1) ==> here element 2 is in the first index (mid = 1) 
+        // checking condition (2 > 3) {false} updating start to mid + 1 (start = 3)
+
+        // Second iteration
+        // start = index(2) , end = index(3) ==> mid = index(2) => value of mid is 3
+        // checking condition (3 > 1) (here we confirm it starts decreasing, so we found a possible peak;
+        // now, we need to explore the left side to find a higher element (potential peak))
+        // updating end = mid
+
+
+        //Third iteration (Here we found the decreasing element now we need to check whether another possible element is there ?)
+        // start = index(2) , end  = index(2)
+
         // output : 2
         int result = peak_index(arr);
         
@@ -26,8 +40,8 @@ public class Leetcode_162 {
             }
         }
 
-        return start;
+        return end;
     }
-    
+     
 }
 
